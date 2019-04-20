66de738fa093fcd6c1495ba32fb1452b42208d52
riceincoke <42928623+riceincoke@users.noreply.github.com>
2019-04-20 23:17:14 +0800

    jdk 动态代理

---
+++ src/main/java/pri/zxx/learndemo/models/dynproxy/proxy/MyInvocationHandler.java
@@ -0,0 +1,25 @@
+package pri.zxx.learndemo.models.dynproxy.proxy;
+
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author Twilight
+ * @desc
+ * @createTime 2019-04-20-22:31
+ */
+public class MyInvocationHandler<T> implements InvocationHandler {
+    T moveable;
+
+    public MyInvocationHandler(T moveable) {
+        this.moveable = moveable;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("代理对象执行：" + method.getName());
+        Object invoke = method.invoke(moveable, args);
+        return invoke;
+    }
+}
