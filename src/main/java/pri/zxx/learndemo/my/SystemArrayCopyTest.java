8d1fe4e956ac8cbd30a810a26962142f054567db
riceincoke <42928623+riceincoke@users.noreply.github.com>
2019-11-08 23:28:33 +0800

    约瑟夫环 真是醉了

--- src/main/java/pri/zxx/learndemo/my/SystemArrayCopyTest.java
+++ src/main/java/pri/zxx/learndemo/my/SystemArrayCopyTest.java
@@ -1,12 +1,23 @@
 package pri.zxx.learndemo.my;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author Twilight
  * @desc 使用system.arraycopy复制数组
  * @createTime 2019-11-08-17:06
  */
 public class SystemArrayCopyTest {
-    public static void systemArrCp() {
+    /**
+     * system.arraycopy 方法
+     */
+    @Test
+    public void systemArrCp() {
         //定义需要删除的位置
         int index = 4;
         //定义数组
@@ -25,7 +36,46 @@ public class SystemArrayCopyTest {
         }
     }
 
-    public static void main(String[] args) {
-        systemArrCp();
+    @Test
+    public void yuesefu() {
+        this.resolve(3, 40);
+    }
+
+    /**
+     * 约瑟夫环
+     */
+    public void resolve(Integer read, Integer menbers) {
+        //报数位置 read
+        //人数
+        List<String> strings = IntStream.range(1, menbers + 1).mapToObj(String::valueOf).collect(Collectors.toList());
+        do {
+            ArrayList<String> index = new ArrayList<>();
+            System.out.println("str:" + strings.toString());
+            for (int i = 1; i <= strings.size(); i++) {
+                if (i % read == 0) {
+                    index.add(strings.get(i - 1));
+                }
+            }
+            //删除需要删除的数据
+            int checkReload = strings.size() % read;
+            System.out.println("size: " + strings.size() + " ,   checkReload : " + checkReload);
+            System.out.println("--------------------------------------------------");
+            strings.removeAll(index);
+            index.clear();
+            if (checkReload != 0) {
+                //如果第一位报数不为1 则重组 头元素
+                strings = this.reloadArray(strings, checkReload);
+            }
+        } while (strings.size() > (read - 1));
+        System.out.println("剩下的人位置 ：" + strings);
+    }
+
+    //重组数组
+    private List<String> reloadArray(List<String> strings, int checkReload) {
+        List<String> pre = strings.subList(strings.size() - checkReload, strings.size());
+        List<String> next = strings.subList(0, strings.size() - checkReload);
+        pre.addAll(next);
+        strings = pre;
+        return strings;
     }
 }
