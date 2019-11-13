a4a5b4eb1012af1cda3138ba475dc06aabd81669
riceincoke <42928623+riceincoke@users.noreply.github.com>
2019-11-13 20:22:01 +0800

    约瑟夫环 真是醉了

--- src/main/java/pri/zxx/learndemo/nomaldemo/PcAction.java
+++ src/main/java/pri/zxx/learndemo/nomaldemo/PcAction.java
@@ -4,16 +4,13 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 
-/**
- * @author Twilight
- */
-public class PcAction {
+public class PCAction {
 
     public static void load() throws IOException {
-        String strUrl = "https://baijiahao.baidu.com/s?id=1589546094825996322&wfr=spider&for=pc";
+        String strurl = "https://baijiahao.baidu.com/s?id=1589546094825996322&wfr=spider&for=pc";
         byte[] cons = new byte[0];
         try {
-            URL url = new URL(strUrl);
+            URL url = new URL(strurl);
             URLConnection conn = url.openConnection();
             InputStream is = conn.getInputStream();
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -38,6 +35,6 @@ public class PcAction {
     }
 
     public static void main(String[] args) throws IOException {
-        PcAction.load();
+        PCAction.load();
     }
 }
