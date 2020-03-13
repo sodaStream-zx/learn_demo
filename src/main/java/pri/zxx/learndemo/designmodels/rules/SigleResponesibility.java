3808a5d422240f548122bd3f606e80f3532fc107
riceincoke <42928623+riceincoke@users.noreply.github.com>
2020-03-13 22:37:52 +0800

    undertow

--- src/main/java/pri/zxx/learndemo/designmodels/rules/SigleResponesibility.java
+++ src/main/java/pri/zxx/learndemo/designmodels/rules/SigleResponesibility.java
@@ -1,8 +1,8 @@
-package pri.zxx.learndemo.designmodels.sigleres;
+package pri.zxx.learndemo.designmodels.rules;
 
 /**
  * @author Twilight
- * @desc
+ * @desc 单一职责原则
  * @createTime 2020-03-13-22:28
  */
 public class SigleResponesibility {
@@ -18,6 +18,7 @@ public class SigleResponesibility {
 class MachineRun {
     /**
      * 在方法级别上遵守单一职责原则
+     * 但是在需要单独处理某种情况需要改动该方法
      */
     public void run(String name) {
         System.out.println(name + "开始运行");
